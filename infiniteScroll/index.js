c1485467c93dbb95a159b1ca97942833ae587235
최 규식 <kuidoli@jinhak.com>
2022-11-23 22:41:25 +0900

    add comment

--- infiniteScroll/index.js
+++ infiniteScroll/index.js
@@ -8,6 +8,14 @@ const debounce = function(func, delay) {
     return function(event) {
         clearTimeout(timeoutId)
         timeoutId = setTimeout(func.bind(null, event), delay)
+        // 요청이 들어올때 마다 timeoutId 가 생성되는데
+        // 이 timeoutId 는 이후 요청에 의해 clearTimeout 으로 지워지고
+        // 새로운 timeoutId 가 생성된다
+        // 이게 반복되면서 마지막 요청만 clearTimeout 에 걸리지 않고 남아서 동작하게 된다
+        //
+        // 만약 scroll 을 delay 시간 이내에 멈추지 않고 계속 하면
+        // 매번 다음 요청의 clearTimeout 으로 지워져서
+        // func 는 동작하지 않는다
     }
 }
 
